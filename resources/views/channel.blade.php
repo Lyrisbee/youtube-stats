79cbf77260dd917fd53845c30b85eea4e844353e
lyrisbee <xicadfta@gmail.com>
2020-02-16 17:07:05 +0800

    fix redirect error

--- resources/views/channel.blade.php
+++ resources/views/channel.blade.php
@@ -21,56 +21,58 @@
             @endcomponent
             <div class="centerbar bg-blanchedalmond">
                 <div class="mt-4">
-                    <table class="table-fixed mt-4">
-                        <div class="paginate text-center">
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination justify-content-center">
-                                    <li class="page-item {{  ($channel->playlists->currentPage() <= 1) ? 'disabled' : ''  }}">
-                                        <a class="page-link" href="{{  route('playlists', $channel->uid) . '?page=1'}}" aria-label="Previous">
-                                            <span aria-hidden="true">&laquo;</span>
-                                            <span class="sr-only">Previous</span>
-                                        </a>
-                                    </li>
-                                    @if ($channel->playlists->currentPage() > 1)
-                                    <li class="page-item">
-                                        <a class="page-link" href="{{  route('playlists', $channel->uid) . '?page=' . ($channel->playlists->currentPage() - 1)}}">
-                                            {{  $channel->playlists->currentPage() - 1  }}
-                                        </a>
-                                    </li>
-                                    @endif
-                                    <li class="page-item active"><a class="page-link" href="#">{{  $channel->playlists->currentPage()  }}</a></li>
-                                    @if ($channel->playlists->currentPage() < $channel->playlists->lastPage())
-                                        <li class="page-item">
-                                            <a class="page-link" href="{{  route('playlists', $channel->uid) . '?page=' . ($channel->playlists->currentPage() + 1)}}">
-                                                {{  $channel->playlists->currentPage() + 1  }}
+                    @if (isset($channel->playlists))
+                        <table class="table-fixed mt-4">
+                            <div class="paginate text-center">
+                                <nav aria-label="Page navigation example">
+                                    <ul class="pagination justify-content-center">
+                                        <li class="page-item {{  ($channel->playlists->currentPage() <= 1) ? 'disabled' : ''  }}">
+                                            <a class="page-link" href="{{  route('playlists', $channel->uid) . '?page=1'}}" aria-label="Previous">
+                                                <span aria-hidden="true">&laquo;</span>
+                                                <span class="sr-only">Previous</span>
                                             </a>
                                         </li>
-                                    @endif
-                                    <li class="page-item {{  ($channel->playlists->currentPage() >= $channel->playlists->lastPage()) ? 'disabled' : ''  }}">
-                                         <a class="page-link" href="{{  route('playlists', $channel->uid) . '?page=' . ($channel->playlists->lastPage())}}" aria-label="Next">
-                                            <span aria-hidden="true">&raquo;</span>
-                                            <span class="sr-only">Next</span>
+                                        @if ($channel->playlists->currentPage() > 1)
+                                        <li class="page-item">
+                                            <a class="page-link" href="{{  route('playlists', $channel->uid) . '?page=' . ($channel->playlists->currentPage() - 1)}}">
+                                                {{  $channel->playlists->currentPage() - 1  }}
+                                            </a>
+                                        </li>
+                                        @endif
+                                        <li class="page-item active"><a class="page-link" href="#">{{  $channel->playlists->currentPage()  }}</a></li>
+                                        @if ($channel->playlists->currentPage() < $channel->playlists->lastPage())
+                                            <li class="page-item">
+                                                <a class="page-link" href="{{  route('playlists', $channel->uid) . '?page=' . ($channel->playlists->currentPage() + 1)}}">
+                                                    {{  $channel->playlists->currentPage() + 1  }}
+                                                </a>
+                                            </li>
+                                        @endif
+                                        <li class="page-item {{  ($channel->playlists->currentPage() >= $channel->playlists->lastPage()) ? 'disabled' : ''  }}">
+                                            <a class="page-link" href="{{  route('playlists', $channel->uid) . '?page=' . ($channel->playlists->lastPage())}}" aria-label="Next">
+                                                <span aria-hidden="true">&raquo;</span>
+                                                <span class="sr-only">Next</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            @foreach ($channel->playlists as $ind => $video)
+                                @if ($ind%3 === 0)
+                                    <tr>
+                                @endif
+                                    <td class="pr-4 pl-4">
+                                        <a href="#">
+                                            <p><img class="yt-img" src="https://i.ytimg.com/vi/{{$video->uid}}/hqdefault.jpg" alt="thumbnail"></p>
+                                            <p class="text-truncate-height" title="{{$video->name}}">{{$video->name}}</p>
+                                            <p>觀看次數：{{  number_format_short($video->views)  }} 次，{{  $video->create_at  }}</p>
                                         </a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                        @foreach ($channel->playlists as $ind => $video)
-                            @if ($ind%3 === 0)
-                                <tr>
-                            @endif
-                                <td class="pr-4 pl-4">
-                                    <a href="#">
-                                        <p><img class="yt-img" src="https://i.ytimg.com/vi/{{$video->uid}}/hqdefault.jpg" alt="thumbnail"></p>
-                                        <p class="text-truncate-height" title="{{$video->name}}">{{$video->name}}</p>
-                                        <p>觀看次數：{{  number_format_short($video->views)  }} 次，{{  $video->create_at  }}</p>
-                                    </a>
-                                </td>
-                            @if ($ind%3 === 2)
-                                </tr>
-                            @endif
-                        @endforeach
-                    </table>
+                                    </td>
+                                @if ($ind%3 === 2)
+                                    </tr>
+                                @endif
+                            @endforeach
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,20 +11,20 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-info">
-            <a class="navbar-brand" href="#">Youtuber Stats</a>
+            <a class="navbar-brand" href="/">Youtuber Stats</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-5 mr-auto">
                     <li class="nav-item active mr-2">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                     </li>
+                    <!--
                     <li class="nav-item">
                         <a class="nav-link" href="#">Videos</a>
                     </li>
-                    </div>
-                </li>
+                    -->
                 </ul>
                 <!--
                 <form class="form-inline my-2 my-lg-0">
